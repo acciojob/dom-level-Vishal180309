66256b20af3d90bc2470f374b9e6a6b2b5dd9b7d
Vishal180309 <2305112110077@paruluniversity.ac.in>
2025-03-01 08:28:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function getDomLevel(element) {
+    let level = 0;
+    while (element) {
+        element = element.parentNode;
+        level++;
+    }
+    return level;
+}
+
+const levelElement = document.getElementById("level");
+const domLevel = getDomLevel(levelElement);
+
+alert(`The level of the element is: ${domLevel}`);
